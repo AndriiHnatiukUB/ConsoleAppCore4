2a456d101d6a57d1eed0bbbafa7e9fd934979fa6
andrii.hnatiuk <andrii.hnatiuk@unity-bars.com>
2018-03-05 17:09:26 +0200

    Add project files.

---
+++ ConsoleAppCore4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppCore4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
